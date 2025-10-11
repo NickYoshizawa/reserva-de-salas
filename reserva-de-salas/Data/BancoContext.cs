5e604d6d2f570bebd8d90e37e45c80730404b1cf
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2025-10-11 16:17:06 -0300

    Criado Entidade usuario

---
+++ reserva-de-salas/Data/BancoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using reserva_de_salas.Models;
+
+namespace reserva_de_salas.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
